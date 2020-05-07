ccc49b79c2805dda4b430bdf571c54c9c0177e8d
narias1999 <nicolasarias870@gmail.com>
2020-05-06 22:48:10 -0500

    improving logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
+// Make sure to have implemented the css classes and add the respective image before adding a color.
 const colors = ['blue', 'black', 'green', 'orange', 'red'];
+
 const $colorsContainer = document.getElementById('colorsContainer');
 const $image = document.querySelector('img');
+const $imgBox = document.querySelector('.imgBox');
+const $buyButton = document.querySelector('button');
+const $body = document.querySelector('body');
 
 const $buttons = colors.map(generateColorButton);
-$buttons[0].classList.add('selected');
+
+// Initialize image
+onButtonClicked({ target: $buttons[0] }, colors[0]);
 
 function generateColorButton(color) {
   const $button = document.createElement('a');
@@ -15,8 +22,12 @@ function generateColorButton(color) {
 
 function onButtonClicked(event, color) {
   $buttons.forEach(button => button.classList.remove('selected'));
-  event.target.classList.add('selected');  
-  document.querySelector('body').classList.value = color;
-  $image.src = `./assets/img/${color}-nike.png`
+  event.target.classList.add('selected');
+
+  $body.classList.value = `bg-${color}-dark`;
+  $buyButton.classList.value = `bg-${color}`;
+  $imgBox.classList.value = `imgBox bg-${color}`;
+
+  $image.src = `./assets/img/${color}-nike.png`;
   $image.alt = `Zapato deportivo Nike color ${color}, para correr`;
 }
